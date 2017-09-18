51964c06b9ae09b05899f75d98bbba7b2ccab388
edubwitowski <31215451+edubwitowski@users.noreply.github.com>
2017-09-17 22:13:39 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,43 @@
+package com.company;
+
+
+
+    public static void main(String[] args) {
+
+
+        String[] myDogArray;
+
+
+        class myDogArray {
+            public static void main(String[] args) {
+
+                int[] myDogArray;
+
+
+                myDogArray = new int[10];
+
+
+                myDogArray[0] = 1;
+
+                myDogArray[1] = 2;
+
+                myDogArray[2] = 3;
+                myDogArray[3] = 4;
+                myDogArray[4] = 5;
+
+
+                System.out.println("Element at index 0: "
+                        + myDogArray[0]);
+                System.out.println("Element at index 1: "
+                        + myDogArray[1]);
+                System.out.println("Element at index 2: "
+                        + myDogArray[2]);
+                System.out.println("Element at index 3: "
+                        + myDogArray[3]);
+                System.out.println("Element at index 4: "
+                        + myDogArray[4]);
+
+            }
+        }
+    }
+}
